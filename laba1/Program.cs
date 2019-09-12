c47db95df7348fcd21f5c4a05400a94cece8b825
KirillFralov <kirill669@bk.ru>
2019-09-12 10:16:51 +0300

    1.Изменение приветиствия

---
+++ laba1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello",name);
+            Console.ReadLine();
+        }
+    }
+}
